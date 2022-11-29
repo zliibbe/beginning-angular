1d5518d7d6318c9c38cf989a7693bce8682efb30
ZachLiibbe <zach.liibbe@bluestaq.com>
2022-11-29 11:52:12 -0700

    Add event emitter to communicate recipe selected to parent and Input recipe of Recipe type

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { Recipe } from "../../recipe.model";
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
-export class RecipeItemComponent {
+export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe;
+  @Output() recipeSelected = new EventEmitter<void>();
+
+  onRecipeSelected() {
+    this.recipeSelected.emit()
+  }
+
+  ngOnInit() {
+  }
 
 }
