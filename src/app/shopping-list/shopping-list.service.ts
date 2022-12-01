89cc53a0423c8bb38c7edfd7ce9d4a8c03151dd6
ZachLiibbe <zach.liibbe@bluestaq.com>
2022-12-01 16:15:12 -0700

    Add addIngredients method

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -18,4 +18,12 @@ export class ShoppingListService {
     this.ingredientsChanged.emit(this.ingredients.slice())
   }
 
+  addIngredients(ingredients: Ingredient[]) {
+    // for (let ingredient of ingredients){
+    //   this.addIngredient(ingredient)
+    // }
+    this.ingredients.push(...ingredients)
+    this.ingredientsChanged.emit(this.ingredients.slice())
+  }
+
 }
