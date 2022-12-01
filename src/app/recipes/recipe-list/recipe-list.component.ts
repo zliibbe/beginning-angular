9b8ec905ce1a44e1567fe1db5103954f041ca697
ZachLiibbe <zach.liibbe@bluestaq.com>
2022-12-01 15:02:53 -0700

    Add Recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Recipe } from "../recipe.model";
+import { RecipeService } from "../recipe.service";
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,18 +9,13 @@ import { Recipe } from "../recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'this is a recipe description', 'https://www.cookipedia.co.uk/wiki/images/e/ea/Gazpacho_ligero_con_mostaza_recipe.jpg'),
-    new Recipe('Another Test Recipe', 'Wow! this is also a recipe description', 'https://www.cookipedia.co.uk/wiki/images/e/ea/Gazpacho_ligero_con_mostaza_recipe.jpg')
-  ];
+  recipes: Recipe[]
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe)
+  constructor(private recipeService: RecipeService) {
   }
 
-  constructor() {  }
-
-  ngOnInit() { }
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes()
+  }
 
 }
