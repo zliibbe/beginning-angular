10558e91dd95cf20f5aa0843b24ebbd3d1643c95
ZachLiibbe <zach.liibbe@bluestaq.com>
2022-12-01 16:17:16 -0700

    Inject Shopping List Service into Recipe service to allow addition of ingredients from recipe detail to shopping list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from "../recipe.model";
+import { RecipeService } from "../recipe.service";
+import { Ingredient } from "../../shared/ingredient.model";
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,4 +10,11 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeDetailComponent {
   @Input() recipe: Recipe
+
+  constructor(private recipeService: RecipeService) {
+  }
+
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients)
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
 import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>()
 
@@ -28,7 +30,14 @@ export class RecipeService {
       ])
   ];
 
+  constructor(private shoppingListService: ShoppingListService) {
+  }
+
   getRecipes() {
     return this.recipes.slice() //.slice gives us a copy of the array
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients)
   }
 }
